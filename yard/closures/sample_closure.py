ec6961357ab71c922321eb5334d0d40b93d96d38
Samurais <hailiang.hl.wang@gmail.com>
2013-09-15 16:03:31 +0800

    add closure

---
+++ yard/closures/sample_closure.py
@@ -0,0 +1,12 @@
+#Here is a closure in python:
+def makeInc(x):
+    def inc(y):
+     # x is "closed" in the definition of inc
+        return y + x
+    return inc
+
+inc5 = makeInc(5)
+inc10 = makeInc(10)
+
+print inc5 (5) # returns 10
+print inc10(5) # returns 15
